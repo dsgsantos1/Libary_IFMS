49debc4fa8ba8f331a8e4a35bbba83b51e080486
Daniel dos Santos Gonçalves <68562083+dsgsantos1@users.noreply.github.com>
2021-06-23 22:50:43 -0400

    Add files via upload

---
+++ DataFormato.java
@@ -0,0 +1,16 @@
+package biblioteca.modelo;
+
+import java.util.Date;
+
+public class DataFormato {
+	
+	private Date dataAtual;
+
+	public Date getDataAtual() {
+		return dataAtual;
+	}
+
+	public void setDataAtual(Date dataAtual) {
+		this.dataAtual = dataAtual;
+	} 
+}
---
+++ Emprestimo.java
@@ -0,0 +1,24 @@
+package biblioteca.modelo;
+import java.util.Date;
+
+public class Emprestimo {
+
+	private Date dataEmprestimo, dataDevolucao;
+
+	public Date getDataEmprestimo() {
+		return dataEmprestimo;
+	}
+
+	public void setDataEmprestimo(Date dataEmprestimo) {
+		this.dataEmprestimo = dataEmprestimo;
+	}
+
+	public Date getDataDevolucao() {
+		return dataDevolucao;
+	}
+
+	public void setDataDevolucao(Date dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
+	}
+	
+}
